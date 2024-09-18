e2009dcd139556d95e5d4206b454ee3b7e618e09
asus test <msa-99@outlook.com>
2024-09-18 22:10:29 +0600

    dsu intersaction

---
+++ disJoinSetUnion.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+ Duti set er moddye intersaction korle zodi faka/empty set pawya zay sei faka/empty set i hocce disjoin union set.
+
+
+   
+ 
+*/
+int main(){
+    
+    return 0;
+}
+
+/*
+sample input output
+input
+
+
+output
+
+*/
