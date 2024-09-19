2c106ab955a3be24a72821eb90b2df6eebc73c79
asus test <msa-99@outlook.com>
2024-09-19 14:53:05 +0600

    MST

---
+++ MST.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+MST ekti graph er multiple path and edge bad diye graph er total cost komiye ane and graph tike Tree te convert kore.  Zehetu MST multiple edge and path remove kore dey, sehtu kuno cycle create hoy na.
+*/
+int main(){
+    
+    return 0;
+}
