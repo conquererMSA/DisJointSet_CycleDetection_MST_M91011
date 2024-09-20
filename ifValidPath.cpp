9d2a0047d13216a20e7c1fd4238ec5b96a2920f3
asus test <msa-99@outlook.com>
2024-09-20 21:04:02 +0600

    ifValidPathExist leetCode

--- ifValidPath.cpp
+++ ifValidPath.cpp
@@ -1,5 +1,26 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    return 0;
-}
+/*
+https://leetcode.com/problems/find-if-path-exists-in-graph/
+*/
+class Solution {
+public:
+    vector<int> v[200000];
+    bool vis[200005];
+    void dfs(int s){
+        vis[s]=true;
+        for(int child:v[s]){
+            if(!vis[child]){
+                dfs(child);
+            }
+        }
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int s, int d) {
+        for(vector<int> i:edges){
+            int a=i[0];
+            int b=i[1];
+            v[a].push_back(b);
+            v[b].push_back(a);
+        }
+        dfs(s);
+        return vis[d];
+    }
+};
